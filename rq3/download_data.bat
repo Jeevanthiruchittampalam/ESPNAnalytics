1092fc0c9b03df8a39c74745f3a8ba60c9846955
Curtis Heinzl <curtis.heinzl@gmail.com>
2024-04-21 13:14:35 -0400

    Delete duplicate dataset & move script for rq3 data to rq3 folder

--- rq3/download_data.bat
+++ rq3/download_data.bat
@@ -1,10 +1,6 @@
-mkdir -p data/rq3
-cd data/rq3
+mkdir data
+cd data
 curl -Lo players.json "https://figshare.com/ndownloader/files/15073721"
 curl -Lo teams.json "https://figshare.com/ndownloader/files/15073697"
 curl -Lo events.zip "https://figshare.com/ndownloader/files/14464685"
 curl -Lo matches.zip "https://figshare.com/ndownloader/files/14464622"
-unzip events.zip -d events
-unzip matches.zip -d matches
-rm events.zip
-rm matches.zip
--- rq3/download_data.sh
+++ rq3/download_data.sh
@@ -1,6 +1,10 @@
-mkdir data\rq3
-cd data\rq3
+mkdir -p data
+cd data
 curl -Lo players.json "https://figshare.com/ndownloader/files/15073721"
 curl -Lo teams.json "https://figshare.com/ndownloader/files/15073697"
 curl -Lo events.zip "https://figshare.com/ndownloader/files/14464685"
 curl -Lo matches.zip "https://figshare.com/ndownloader/files/14464622"
+unzip events.zip -d events
+unzip matches.zip -d matches
+rm events.zip
+rm matches.zip
